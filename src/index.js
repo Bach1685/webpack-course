3730bd3cd08b8025c9ab13c09a289b72782d035d
Bach1685 <alekseimatyuhin@gmail.com>
2022-12-21 21:11:28 +0300

    access to json file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Post from './Post'
 import './styles/styles.css'
+import json from './assets/fileJson.json' //вот так легко можно получить доступ к json файлу
+console.log('json', json)
 
 const post = new Post('webpack post title')
 
