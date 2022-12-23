0bd5eae8dceabf430837727d5cceb5ad4fb3490f
Bach1685 <alekseimatyuhin@gmail.com>
2022-12-23 18:21:24 +0300

    use png

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Post from './Post'
 import './styles/styles.css'
-import json from './assets/fileJson.json' //вот так легко можно получить доступ к json файлу
-console.log('json', json)
+import json from './assets/json.json' //вот так легко можно получить доступ к json файлу
+import WebpackLogo from './assets/webpack-logo.png'
 
-const post = new Post('webpack post title')
+const post = new Post('webpack post title', WebpackLogo)
 
 console.log(post.toString())
